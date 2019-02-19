75634af413ca378756a8f3562a826a4084f65f1a
lucasws <weberxw@gmail.com>
2019-02-19 14:41:42 -0300

    Query Filtering and ordering working

--- myrest/myapp/views.py
+++ myrest/myapp/views.py
@@ -7,9 +7,11 @@ from .serializers import ResumeSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_condition import Or
-
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, OAuth2Authentication
 from rest_framework.authentication import SessionAuthentication
+from django_filters import rest_framework as filters
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 
 
 # Create your views here.
@@ -20,4 +22,9 @@ class ResumeList(generics.ListCreateAPIView):
     serializer_class = ResumeSerializer
     authentication_classes = [OAuth2Authentication, SessionAuthentication]
     permission_classes = [Or(IsAdminUser, TokenHasReadWriteScope)]
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = ('name',)
+
 
